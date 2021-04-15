0f85c65fba91ba2c723f5971d02b3ea3b67247c0
ShiccaRorl <shicca.rorl@gmail.com>
2021-04-16 06:43:15 +0900

    20210416

---
+++ site/config.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+
+class Config():
+    def __init__(self):
+        # DB
+        self.db_path = "./../../db.sqlite3"
+        
+        # download
+        self.download_path = "C:/Users/ban/Downloads/"
+        
--- site/core.py
+++ site/core.py
@@ -11,35 +11,57 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 
+form config from Config
+
 class Core():
     def __init__(self)
+        config = Config()
         Base = automap_base()
 
         # engine, suppose it has two tables 'user' and 'address' set up
-        engine = create_engine("sqlite:///db.sqlite3")
+        engine = create_engine(config.db_path)
+        # 個人情報の塊なのでGITの外に設置
 
         # reflect the tables
         Base.prepare(engine, reflect=True)
 
         # mapped classes are now created with names by default
         # matching that of the table name.
-        affiliate_video = Base.classes.Affiliate_Video_affiliate_video
-        affiliate_video_pic = Base.classes.Affiliate_Video_affiliate_video_pic
+        seed = Base.classes.seed
+        site = Base.classes.site
 
         session = Session(engine)
         
-        def save(self, seed):
+        config.download_path
         
-            i = 0
-            while i <= 5:
-                try:
-                    time.sleep(1)
-                    session.commit()
-                    time.sleep(1)
-                    i = 6 + 1
-                except:
-                    time.sleep(5)
-                    i = i + 1
+    def seed_save(self):
+        for dir in glob.glob('C:\\Users\\ban\\Downloads\\amazon*.html'):
+            with open(dir, 'r', encoding="utf-8") as f:
+                self.seed1 = f.read()
+            self.seed1 = self.seed1.replace("\n", "")
+            self.seed1 = self.seed1.replace("\r", "")
+            
+            #dmm = Analysis_MGS(self.seed1)
+            session = Session(engine)
+            
+            self.file_list.append(dmm.code)
+            seed = session.query(self.seed).filter(self.seed)
+            session.add(self.seed.seed = self.seed)
+            save(session)
+            
+            self.削除する(dir)
+    def save(self, session):
         
-        def load(self):
+        i = 0
+        while i <= 5:
+            try:
+                time.sleep(1)
+                session.commit()
+                time.sleep(1)
+                i = 6 + 1
+            except:
+                time.sleep(5)
+                i = i + 1
+        
+    def load(self):
         
