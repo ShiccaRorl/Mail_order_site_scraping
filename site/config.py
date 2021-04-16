d9ff46f3755890c94fb251161881f35ed0d0c40e
ShiccaRorl <shicca.rorl@gmail.com>
2021-04-16 12:16:18 +0900

    r

--- site/config.py
+++ site/config.py
@@ -3,7 +3,7 @@
 class Config():
     def __init__(self):
         # DB
-        self.db_path = "./db.sqlite3"
+        self.db_path = "sqlite:///./../../db.sqlite3"
         
         # download
         self.download_path = "C:/Users/ban/Downloads/"
