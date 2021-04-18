7a5e98ab2f1793c533a790763a7b53d1158e707b
ShiccaRorl <shicca.rorl@gmail.com>
2021-04-19 08:36:52 +0900

    filename

---
+++ site/to_取り込み.cmd
@@ -0,0 +1,7 @@
+rem python directory_seed_アマゾン.py
+rem python directory_seed_ストアーズ.py
+rem python directory_seed_ヤフオク.py
+rem python directory_seed_ラクマ.py
+rem python directory_seed_楽天.py
+
+python core.py
