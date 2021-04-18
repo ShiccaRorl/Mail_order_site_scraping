1b75a91c267b765407ddd4eba787a39abd8b6fb8
ShiccaRorl <shicca.rorl@gmail.com>
2021-04-19 08:24:42 +0900

    取り込み失敗

--- site/core.py
+++ site/core.py
@@ -33,28 +33,116 @@ class Core():
         #session = Session(self.engine)
         
         #config.download_path
-    
+        self.name = ""
+        self.dir = ""
+        self.siteID = 0
+        
     def 削除する(self, code):
         print(f"{code} MGS 削除します")
         os.remove(code)
     
-    def seed_save(self):
-        for dir in glob.glob(f'{self.config.download_path}amazon*.html'):
+    def seed_save_add():
+        for dir in glob.glob(f"{self.dir}{self.name}*.html"):
             if dir != None:
-                with open(dir, 'r', encoding="utf-8") as f:
-                    self.seed1 = f.read()
-                self.seed1 = self.seed1.replace("\n", "")
-                self.seed1 = self.seed1.replace("\r", "")
-            
+                print(dir)
+                self.seed1 = self.load(dir)
                 #dmm = Analysis_MGS(self.seed1)
                 session = Session(self.engine)
+
+                self.seed = session.query(self.seed)
+                session.add(self.seed(siteID=self.siteID,
+                                      seed=self.seed1,
+                                      create_at=datetime.datetime.now(),))
+                
+                self.save(session)
             
-                self.seed = session.query(self.seed).filter(self.seed)
-                session.add(self.seed.seed == self.seed)
+                self.削除する(dir)
+    
+    def seed_save_amazon(self):
+        for dir in glob.glob(f'{self.config.download_path}amazon*.html'):
+            if dir != None:
+                print(dir)
+                self.seed1 = self.load(dir)
+                #dmm = Analysis_MGS(self.seed1)
+                session = Session(self.engine)
+
+                self.seed = session.query(self.seed)
+                session.add(self.seed(siteID=0,
+                                      seed=self.seed1,
+                                      create_at=datetime.datetime.now(),))
+                
                 self.save(session)
             
                 self.削除する(dir)
                 
+    def seed_save_stores(self):
+        for dir in glob.glob(f'{self.config.download_path}stores*.html'):
+            if dir != None:
+                print(dir)
+                self.seed1 = self.load(dir)
+                #dmm = Analysis_MGS(self.seed1)
+                session = Session(self.engine)
+
+                self.seed = session.query(self.seed)
+                session.add(self.seed(siteID=4,
+                                      seed=self.seed1,
+                                      create_at=datetime.datetime.now(),))
+                
+                self.save(session)
+            
+                self.削除する(dir)
+                
+    def seed_save_rakuten_rakuma(self):
+        for dir in glob.glob(f'{self.config.download_path}rakuten_rakuma*.html'):
+            if dir != None:
+                print(dir)
+                self.seed1 = self.load(dir)
+                #dmm = Analysis_MGS(self.seed1)
+                session = Session(self.engine)
+
+                self.seed = session.query(self.seed)
+                session.add(self.seed(siteID=3,
+                                      seed=self.seed1,
+                                      create_at=datetime.datetime.now(),))
+                
+                self.save(session)
+            
+                self.削除する(dir)
+
+    def seed_save_rakuten(self):
+        for dir in glob.glob(f'{self.config.download_path}rakuten*.html'):
+            if dir != None:
+                print(dir)
+                self.seed1 = self.load(dir)
+                #dmm = Analysis_MGS(self.seed1)
+                session = Session(self.engine)
+
+                self.seed = session.query(self.seed)
+                session.add(self.seed(siteID=1,
+                                      seed=self.seed1,
+                                      create_at=datetime.datetime.now(),))
+                
+                self.save(session)
+            
+                self.削除する(dir)
+             
+    def seed_save_auctions_yahoo(self):
+        for dir in glob.glob(f'{self.config.download_path}auctions_yahoo*.html'):
+            if dir != None:
+                print(dir)
+                self.seed1 = self.load(dir)
+                #dmm = Analysis_MGS(self.seed1)
+                session = Session(self.engine)
+
+                self.seed = session.query(self.seed)
+                session.add(self.seed(siteID=2,
+                                      seed=self.seed1,
+                                      create_at=datetime.datetime.now(),))
+                
+                self.save(session)
+            
+                self.削除する(dir)  
+
     def save(self, session):
         
         i = 0
@@ -68,12 +156,20 @@ class Core():
                 time.sleep(5)
                 i = i + 1
         
-    def load(self):
-        print("load")
+    def load(self, dir):
+        with open(dir, 'r', encoding="utf-8") as f:
+            self.seed1 = f.read()
+        self.seed1 = self.seed1.replace("\n", "")
+        self.seed1 = self.seed1.replace("\r", "")
+        return self.seed1
         
         
 if __name__ == '__main__':
     
     core = Core()
-    core.seed_save()
+    core.seed_save_amazon()
+    core.seed_save_stores()
+    core.seed_save_rakuten_rakuma()
+    core.seed_save_rakuten()
+    core.seed_save_auctions_yahoo()
     print(core)
