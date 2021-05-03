6852a34c199b7209fe67dc87edda3b2e6c600e23
ShiccaRorl <shicca.rorl@gmail.com>
2021-05-03 20:49:51 +0900

    データベースが無かったら作る様にする。

---
+++ site/setting.py
@@ -0,0 +1,39 @@
+
+from sqlalchemy import *
+from sqlalchemy.orm import *
+from sqlalchemy.ext.declarative import declarative_base
+
+from config import Config
+
+# mysqlのDBの設定
+"""
+DATABASE = 'mysql://%s:%s@%s/%s?charset=utf8' % (
+    "user_name",
+    "password",
+    "host_ip",
+    "db_name",
+)
+"""
+self.config = Config()
+
+
+
+ENGINE = create_engine(
+    self.config.db_path,
+    encoding = "utf-8",
+    echo=True # Trueだと実行のたびにSQLが出力される
+)
+
+# Sessionの作成
+session = scoped_session(
+  # ORM実行時の設定。自動コミットするか、自動反映するなど。
+　　　　sessionmaker(
+　　　　　　　　autocommit = True,
+　　　　　　　　autoflush = True,
+　　　　　　　　bind = ENGINE
+　　　　)
+)
+
+# modelで使用する
+Base = declarative_base()
+Base.query = session.query_property()
--- site/to_取り込み.ps1
+++ site/to_取り込み.ps1
@@ -2,3 +2,4 @@ PowerShell -Command python core.py
 
 PowerShell -Command python directory_seed_ヤフオク.py
 
+PowerShell -Command python excel_db.py
