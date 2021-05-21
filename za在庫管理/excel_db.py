daf2e35806999689980eb7e105667ae3214b2697
ShiccaRorl <shicca.rorl@gmail.com>
2021-05-21 20:24:23 +0900

    yoru

--- za在庫管理/excel_db.py
+++ za在庫管理/excel_db.py
@@ -1,6 +1,9 @@
 import openpyxl
-wb = openpyxl.load_workbook('C:\\temp\\foo.xlsx')
 
-ws = wb.worksheets[0]
+excel_path1 = "C:/Users/user/Downloads/バックアップ/プログラム/バックアップ/保存/2020年9月在庫表.xlsx"
+excel_path2 = "./2020年9月在庫表.xlsx"
+wb = openpyxl.load_workbook(excel_path2)
+
+ws = wb["マスター"]
 wb.copy_worksheet(ws)
-wb.save('C:\\temp\\foo2.xlsx')
+wb.save("./Excel_db.xlsx")
