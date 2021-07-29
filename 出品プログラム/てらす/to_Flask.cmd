db605c9501f43c52202e9708b7b06860fb09cc9b
ShiccaRorl <shicca.rorl@gmail.com>
2021-07-29 14:49:09 +0900

    dd

--- 出品プログラム/てらす/to_Flask.cmd
+++
@@ -1,6 +0,0 @@
-:loop
-
-powershell -NoProfile -ExecutionPolicy Unrestricted .\to_Flask.ps1
-
-timeout 1800
-goto :loop
--- 出品プログラム/てらす/to_Flask.ps1
+++
@@ -1,7 +0,0 @@
-$i = 1
-while ($i -le 5){
-
-PowerShell -Command python Flask_index.py
-
-Sleep 5
-}
