012009a76085b117675c97f3a2861b506e1855ca
ShiccaRorl <37141533+ShiccaRorl@users.noreply.github.com>
2021-08-17 05:04:55 +0900

    ロガー付けてみた。

--- za在庫管理/za_Rakuma_test.py
+++ za在庫管理/za_Rakuma_test.py
@@ -8,6 +8,7 @@
 #from config import Config
 #from directory_seed import Mail_order_site
 
+from logging import getLogger, StreamHandler, DEBUG
 from bs4 import BeautifulSoup
 
 import datetime
@@ -19,6 +20,7 @@ import glob
 import sys
 import subprocess
 import pathlib
+import logging
 
 """
 from sqlalchemy.ext.automap import automap_base
@@ -49,12 +51,30 @@ class Rakuma_Test():
         args = sys.argv
 
         try:
-            self.file_path = args[1]
-            self.file_out = pathlib.Path(args[1])
+            print(len(args))
+            if len(args) == 1:
+                self.file_path = args[0]
+                self.file_out = pathlib.Path(args[0])
+            elif len(args) == 2:
+                self.file_path = args[1]
+                self.file_out = pathlib.Path(args[1])
+            elif len(args) == 3:
+                self.file_path2 = args[2]
+                self.file_out2 = pathlib.Path(args[2])
         except:
             print("データリフレッシュモード")
             self.file_out = pathlib.Path("")
+            self.file_out2 = pathlib.Path("")
             self.ラクマディレクトリ()
+
+        self.logger = getLogger(self.file_path)
+        handler = StreamHandler()
+        handler.setLevel(DEBUG)
+        self.logger.setLevel(DEBUG)
+        self.logger.addHandler(handler)
+        self.logger.propagate = False
+
+        self.logger.debug('始め')
 
         self.main()
 
@@ -69,135 +89,160 @@ class Rakuma_Test():
         self.在庫1商品リスト()
         self.登録出来ていない商品チェック()
         self.出品されているのに在庫0のリスト()
+        if self.file_out2 != None:
+            self.出品の比較する()
 
     def ラクマディレクトリ(self):
-        i = 0
-        for d in glob.glob(ROOT_PATH + "/**/*.txt"):
-            d_path = pathlib.Path(d)
-            print(d_path)
-            if i == 0:
-                with open("./ラクマディレクトリ.txt", 'w', encoding="utf-8") as f:
-                    #f.write(d_path.stem + "\n")
-                    f.write(d_path + "\n")
-                    i = 1
-            else:
-                with open("./ラクマディレクトリ.txt", 'a', encoding="utf-8") as f:
-                    #f.write(d_path.stem + "\n")
-                    f.write(d_path + "\n")
-            print(d)
+        try:
+            i = 0
+            for d in glob.glob(ROOT_PATH + "/**/*.txt", recursive=True):
+                d_path = pathlib.Path(d)
+                print(d_path)
+                if i == 0:
+                    with open("./ラクマディレクトリ.txt", 'w', encoding="utf-8") as f:
+                        #f.write(d_path.stem + "\n")
+                        f.write(d_path + "\n")
+                        i = 1
+                else:
+                    with open("./ラクマディレクトリ.txt", 'a', encoding="utf-8") as f:
+                        #f.write(d_path.stem + "\n")
+                        f.write(d_path + "\n")
+                print(d)
+        except:
+            print("ラクマディレクトリerr")
+            self.logger.debug('ラクマディレクトリerr')
 
     def ラクマ出品一覧(self):
-        with open(self.file_path, 'r', encoding="utf-8") as f:
-            self.seed1 = f.read()
+        try:
+            with open(self.file_path, 'r', encoding="utf-8") as f:
+                self.seed1 = f.read()
 
-        soup = BeautifulSoup(self.seed1, 'html.parser')
-        data = soup.find_all("h4", attrs={"class": "media-heading"})
+            soup = BeautifulSoup(self.seed1, 'html.parser')
+            data = soup.find_all("h4", attrs={"class": "media-heading"})
 
-        # <h4 class="media-heading">新品送料込み　男女兼用　白足袋24.0ｃｍ★成人式、結婚式、卒業式 ATU005</h4>
+            # <h4 class="media-heading">新品送料込み　男女兼用　白足袋24.0ｃｍ★成人式、結婚式、卒業式 ATU005</h4>
 
-        i = 0
-        for d in data:
-            print(d.text)
-            if i == 0:
-                print(str(self.file_out.parent))
-                with open(str(self.file_out.parent) + "\\ラクマ出品一覧.txt", 'w', encoding="utf-8") as f:
-                    f.write(d.text + "\n")
-                    i = 1
-            else:
-                with open(str(self.file_out.parent) + "\\ラクマ出品一覧.txt", 'a', encoding="utf-8") as f:
-                    f.write(d.text + "\n")
+            i = 0
+            for d in data:
+                print(d.text)
+                if i == 0:
+                    print(str(self.file_out.parent))
+                    with open(str(self.file_out.parent) + "\\ラクマ出品一覧.txt", 'w', encoding="utf-8") as f:
+                        f.write(d.text + "\n")
+                        i = 1
+                else:
+                    with open(str(self.file_out.parent) + "\\ラクマ出品一覧.txt", 'a', encoding="utf-8") as f:
+                        f.write(d.text + "\n")
+        except:
+            print("ラクマ出品一覧err")
+            self.logger.debug('ラクマ出品一覧err')
 
     def ラクマ登録２重チェック(self):
-        with open(str(self.file_out.parent) + "/ラクマ出品一覧.txt", 'r', encoding="utf-8") as f:
-            self.seed1 = f.read().split("\n")
-
-        with open(str(self.file_out.parent) + "/ラクマ出品一覧Ruby.txt", 'r', encoding="utf-8") as f:
-            self.seed2 = f.read().split("\n")
-
-        # listを削除する
-        for i in self.seed2:
-            self.seed1.remove(i)
-
-        i = 0
-        for d in self.seed1:
-            print("====２重チェック====")
-            print(d)
-            if i == 0:
-                with open(str(self.file_out.parent) + "\\ラクマ出品一覧２重登録.txt", 'w', encoding="utf-8") as f:
-                    f.write(d + "\n")
-                    i = 1
-            else:
-                with open(str(self.file_out.parent) + "\\ラクマ出品一覧２重登録.txt", 'a', encoding="utf-8") as f:
-                    f.write(d + "\n")
-
-    def 登録出来ていない商品チェック(self):
-
-        with open("./ラクマディレクトリ.txt", 'r', encoding="utf-8") as f:
-            self.seed1 = f.read().split("\n")
-
-        with open(str(self.file_out.parent) + "/ラクマ出品一覧Ruby.txt", 'r', encoding="utf-8") as f:
-            self.seed2 = f.read().split("\n")
-
-        with open(str(self.file_out.parent) + "/ラクマ出品一覧在庫0.txt", 'r', encoding="utf-8") as f:
-            self.seed3 = f.read().split("\n")
-
-        print(self.seed1[0])
-        print(self.seed2[0])
-        print(self.seed3[0])
         try:
+            with open(str(self.file_out.parent) + "/ラクマ出品一覧.txt", 'r', encoding="utf-8") as f:
+                self.seed1 = f.read().split("\n")
+
+            with open(str(self.file_out.parent) + "/ラクマ出品一覧Ruby.txt", 'r', encoding="utf-8") as f:
+                self.seed2 = f.read().split("\n")
+
+            # listを削除する
             for i in self.seed2:
                 self.seed1.remove(i)
 
-            for i in self.seed3:
-                self.seed1.remove(i)
+            i = 0
+            for d in self.seed1:
+                print("====２重チェック====")
+                print(d)
+                if i == 0:
+                    with open(str(self.file_out.parent) + "\\ラクマ出品一覧２重登録.txt", 'w', encoding="utf-8") as f:
+                        f.write(d + "\n")
+                        i = 1
+                else:
+                    with open(str(self.file_out.parent) + "\\ラクマ出品一覧２重登録.txt", 'a', encoding="utf-8") as f:
+                        f.write(d + "\n")
         except:
-            print("対象がないです")
+            print("ラクマ登録２重チェックerr")
+            self.logger.debug('ラクマ登録２重チェックerr')
 
-        i = 0
-        for d in self.seed1:
-            print("====登録出来ていない商品かも====")
-            print(d)
-            if i == 0:
-                with open(str(self.file_out.parent) + "\\登録出来ていない商品かも.txt", 'w', encoding="utf-8") as f:
-                    f.write(d + "\n")
-                    i = 1
-            else:
-                with open(str(self.file_out.parent) + "\\登録出来ていない商品かも.txt", 'a', encoding="utf-8") as f:
-                    f.write(d + "\n")
+    def 登録出来ていない商品チェック(self):
+        try:
+            with open("./ラクマディレクトリ.txt", 'r', encoding="utf-8") as f:
+                self.seed1 = f.read().split("\n")
+
+            with open(str(self.file_out.parent) + "/ラクマ出品一覧Ruby.txt", 'r', encoding="utf-8") as f:
+                self.seed2 = f.read().split("\n")
+
+            with open(str(self.file_out.parent) + "/ラクマ出品一覧在庫0.txt", 'r', encoding="utf-8") as f:
+                self.seed3 = f.read().split("\n")
+
+            print(self.seed1[0])
+            print(self.seed2[0])
+            print(self.seed3[0])
+            try:
+                for i in self.seed2:
+                    self.seed1.remove(i)
+
+                for i in self.seed3:
+                    self.seed1.remove(i)
+            except:
+                print("対象がないです")
+
+            i = 0
+            for d in self.seed1:
+                print("====登録出来ていない商品かも====")
+                print(d)
+                if i == 0:
+                    with open(str(self.file_out.parent) + "\\登録出来ていない商品かも.txt", 'w', encoding="utf-8") as f:
+                        f.write(d + "\n")
+                        i = 1
+                else:
+                    with open(str(self.file_out.parent) + "\\登録出来ていない商品かも.txt", 'a', encoding="utf-8") as f:
+                        f.write(d + "\n")
+        except:
+            print("登録出来ていない商品チェックerr")
+            self.logger.debug('登録出来ていない商品チェックerr')
 
     def 在庫1商品リスト(self):
-        with open("./ラクマディレクトリ.txt", 'r', encoding="utf-8") as f:
-            self.seed1 = f.read().split("\n")
+        try:
+            with open("./ラクマディレクトリ.txt", 'r', encoding="utf-8") as f:
+                self.seed1 = f.read().split("\n")
 
-        for t in self.seed1:
-            i = 0
-            if "在庫1" in t:
-                print("====在庫1====")
-                print(t)
-                if i == 0:
-                    with open(str(self.file_out.parent) + "\\ラクマ出品一覧在庫1.txt", 'w', encoding="utf-8") as f:
-                        f.write(t + "\n")
-                        i = 1
-                else:
-                    with open(str(self.file_out.parent) + "\\ラクマ出品一覧在庫1.txt", 'a', encoding="utf-8") as f:
-                        f.write(t + "\n")
+            for t in self.seed1:
+                i = 0
+                if "在庫1" in t:
+                    print("====在庫1====")
+                    print(t)
+                    if i == 0:
+                        with open(str(self.file_out.parent) + "\\ラクマ出品一覧在庫1.txt", 'w', encoding="utf-8") as f:
+                            f.write(t + "\n")
+                            i = 1
+                    else:
+                        with open(str(self.file_out.parent) + "\\ラクマ出品一覧在庫1.txt", 'a', encoding="utf-8") as f:
+                            f.write(t + "\n")
+        except:
+            print("在庫1商品リストerr")
+            self.logger.debug('在庫1商品リストerr')
 
     def 在庫0商品リスト(self):
-        with open("./ラクマディレクトリ.txt", 'r', encoding="utf-8") as f:
-            self.seed1 = f.read().split("\n")
+        try:
+            with open("./ラクマディレクトリ.txt", 'r', encoding="utf-8") as f:
+                self.seed1 = f.read().split("\n")
 
-        for t in self.seed1:
-            print("====在庫0====")
-            i = 0
-            if "在庫0" in t:
-                print(t)
-                if i == 0:
-                    with open(str(self.file_out.parent) + "\\ラクマ出品一覧在庫0.txt", 'w', encoding="utf-8") as f:
-                        f.write(t + "\n")
-                        i = 1
-                else:
-                    with open(str(self.file_out.parent) + "\\ラクマ出品一覧在庫0.txt", 'a', encoding="utf-8") as f:
-                        f.write(t + "\n")
+            for t in self.seed1:
+                print("====在庫0====")
+                i = 0
+                if "在庫0" in t:
+                    print(t)
+                    if i == 0:
+                        with open(str(self.file_out.parent) + "\\ラクマ出品一覧在庫0.txt", 'w', encoding="utf-8") as f:
+                            f.write(t + "\n")
+                            i = 1
+                    else:
+                        with open(str(self.file_out.parent) + "\\ラクマ出品一覧在庫0.txt", 'a', encoding="utf-8") as f:
+                            f.write(t + "\n")
+        except:
+            print("在庫0商品リストerr")
+            self.logger.debug('在庫0商品リストerr')
 
     def load(self, dir):
         with open(dir, 'r', encoding="utf-8") as f:
@@ -207,31 +252,85 @@ class Rakuma_Test():
         return self.seed1
 
     def 出品されているのに在庫0のリスト(self):
-        with open(str(self.file_out.parent) + "/ラクマ出品一覧Ruby.txt", 'r', encoding="utf-8") as f:
-            self.seed1 = f.read().split("\n")
+        try:
+            with open(str(self.file_out.parent) + "/ラクマ出品一覧Ruby.txt", 'r', encoding="utf-8") as f:
+                self.seed1 = f.read().split("\n")
 
-        with open(str(self.file_out.parent) + "/ラクマ出品一覧在庫0.txt", 'r', encoding="utf-8") as f:
-            self.seed2 = f.read().split("\n")
+            with open(str(self.file_out.parent) + "/ラクマ出品一覧在庫0.txt", 'r', encoding="utf-8") as f:
+                self.seed2 = f.read().split("\n")
 
-        data = []
-        for i in self.seed1:
-            for s in self.seed2:
-                if i == s:
-                    data.append(i)
+            data = []
+            for i in self.seed1:
+                for s in self.seed2:
+                    if i == s:
+                        data.append(i)
 
-        i = 0
-        for d in data:
+            i = 0
+            for d in data:
 
-            print("====出品されているのに在庫0のリスト====")
-            print(d)
+                print("====出品されているのに在庫0のリスト====")
+                print(d)
 
-            if i == 0:
-                with open(str(self.file_out.parent) + "\\出品されているのに在庫0のリスト.txt", 'w', encoding="utf-8") as f:
-                    f.write(d + "\n")
-                    i = 1
-            else:
-                with open(str(self.file_out.parent) + "\\出品されているのに在庫0のリスト.txt", 'a', encoding="utf-8") as f:
-                    f.write(d + "\n")
+                if i == 0:
+                    with open(str(self.file_out.parent) + "\\出品されているのに在庫0のリスト.txt", 'w', encoding="utf-8") as f:
+                        f.write(d + "\n")
+                        i = 1
+                else:
+                    with open(str(self.file_out.parent) + "\\出品されているのに在庫0のリスト.txt", 'a', encoding="utf-8") as f:
+                        f.write(d + "\n")
+        except:
+            print("出品されているのに在庫0のリストerr")
+            self.logger.debug('出品されているのに在庫0のリストerr')
+
+    def 出品の比較する(self):
+        try:
+            # 比較_元
+            with open(str(self.file_out.parent) + "/ラクマ出品一覧.txt", 'r', encoding="utf-8") as f:
+                self.seed1 = f.read().split("\n")
+
+            # 比較先
+            with open(str(self.file_out2.parent) + "/ラクマ出品一覧.txt", 'r', encoding="utf-8") as f:
+                self.seed2 = f.read().split("\n")
+
+            data = []
+            for i in self.seed1:
+                for s in self.seed2:
+                    if i == s:
+                        data.append(i)
+
+            i = 0
+            for d in data:
+
+                print("====出品の比較する====")
+                print(d)
+
+                if i == 0:
+                    with open(str(self.file_out2.parent) + "\\出品の比較する.txt", 'w', encoding="utf-8") as f:
+                        f.write(d + "\n")
+                        i = 1
+                else:
+                    with open(str(self.file_out2.parent) + "\\出品の比較する.txt", 'a', encoding="utf-8") as f:
+                        f.write(d + "\n")
+        except:
+            print("出品の比較するerr")
+            self.logger.debug('出品の比較するerr')
+
+    def get_logger(logger_name, log_file, f_fmt='%(message)s'):
+        """ロガーを取得"""
+        # ロガー作成
+        logger = logging.getLogger(logger_name)
+        logger.setLevel(logging.DEBUG)
+
+        # ファイルハンドラ作成
+        file_handler = logging.FileHandler(
+            log_file, mode='a', encoding='utf-8')
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(logging.Formatter(f_fmt))
+
+        # ロガーに追加
+        logger.addHandler(file_handler)
+
+        return logger
 
 
 if __name__ == '__main__':
