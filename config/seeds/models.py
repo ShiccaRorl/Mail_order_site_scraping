19590ab47d9d5bc2db07baefe954afad36b1593d
ShiccaRorl <shicca.rorl@gmail.com>
2021-09-25 12:28:55 +0900

    yyyyy

--- config/seeds/models.py
+++ config/seeds/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Seeds(models.Model):
-    site_code = models.IntegerField(verbose_name="販売サイト_code", default=0)
-    seeds = models.TextField(verbose_name="ソースデータ", default="")
-    created_at = models.DateTimeField(verbose_name="作成日時", auto_now_add=True)
+    __tablename__ = 'T_Seeds'
+    site_code = models.IntegerField(verbose_name="販売サイト_code", default=0, blank=True, null=True)
+    seeds = models.TextField(verbose_name="ソースデータ", default="", blank=True, null=True)
+    created_at = models.DateTimeField(verbose_name="作成日時", auto_now_add=True, blank=True, null=True)
