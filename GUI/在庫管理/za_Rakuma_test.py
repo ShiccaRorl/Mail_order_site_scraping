6d1befe6af7c06f66b8a8ade500df6ef5b989b5e
ShiccaRorl <shicca.rorl@gmail.com>
2021-10-25 06:23:39 +0900

    i

---
+++ GUI/在庫管理/za_Rakuma_test.py
@@ -0,0 +1,371 @@
+# -*- coding:utf-8 -*-
+
+# 2021/08/07
+# pip install -U sqlalchemy
+# pip install -U BeautifulSoup
+
+
+#from config import Config
+#from directory_seed import Mail_order_site
+
+from logging import getLogger, StreamHandler, DEBUG
+from bs4 import BeautifulSoup
+
+import datetime
+import time
+import os
+import random
+import re
+import glob
+import sys
+import subprocess
+import pathlib
+import logging
+
+"""
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+#from bs4 import BeautifulSoup
+#import subprocess
+
+Base = automap_base()
+
+# engine, suppose it has two tables 'user' and 'address' set up
+#engine = create_engine("sqlite:///db.sqlite3")
+
+# reflect the tables
+#Base.prepare(engine, reflect=True)
+
+# mapped classes are now created with names by default
+# matching that of the table name.
+
+#session = Session(engine)
+"""
+
+# 現本から取り込む
+#ROOT_PATH = 'C:\\Users\\user\\Downloads\\バックアップ\\プログラム\\バックアップ\\保存\\メルカリ'
+#ROOT_PATH = 'R:\\L\\Program\\eclipse\\Python\\Affiliate_RssReadr_7\\www\\DMM_アニメ\\62gbr00015'
+ROOT_PATH = '\\\DESKTOP-FK98SN0\\Users\\Public\\Documents\\吉本さんPCから移動したファイル\\メルカリラクマ出品画像\\メルカリ'
+
+
+class Rakuma_Test():
+    def __init__(self):
+        args = sys.argv
+
+        try:
+            print(len(args))
+            if len(args) == 1:
+                self.file_path = args[0]
+                self.file_out = pathlib.Path(args[0])
+                self.ラクマディレクトリ()
+
+            elif len(args) == 2:
+                self.file_path = args[1]
+                self.file_out = pathlib.Path(args[1])
+
+            elif len(args) == 3:
+                self.file_path2 = args[2]
+                self.file_out2 = pathlib.Path(args[2])
+
+        except:
+            print("データリフレッシュモード")
+            self.file_out = pathlib.Path("")
+            self.file_out2 = pathlib.Path("")
+            # self.ラクマディレクトリ()
+
+        self.logger = getLogger(self.file_path)
+        handler = StreamHandler()
+        handler.setLevel(DEBUG)
+        self.logger.setLevel(DEBUG)
+        self.logger.addHandler(handler)
+        self.logger.propagate = False
+
+        self.logger.debug('始め')
+
+        self.main()
+
+    def main(self):
+        # self.ラクマディレクトリ()
+        self.ラクマ出品一覧()
+
+        subprocess.run("ruby zaラクマ_入力テスト.rb " + str(self.file_out.parent), shell=True, text=True)
+        self.ラクマ登録２重チェック()
+        self.在庫0商品リスト()
+        self.在庫1商品リスト()
+        self.登録出来ていない商品チェック()
+        self.出品されているのに在庫0のリスト()
+        # if self.file_out2 != None:
+        #    self.出品の比較する()
+
+    def ラクマディレクトリ(self):
+        print(ROOT_PATH + "\\**\\*.txt")
+        try:
+            i = 0
+            for d in glob.glob(ROOT_PATH + "\\**\\*.txt", recursive=True):
+                d = d.replace("\\", "/")
+                d_path = pathlib.Path(d)
+                # print(d_path)
+                if i == 0:
+                    with open("./ラクマディレクトリ.txt", 'w', encoding="utf-8") as f:
+                        f.write(d + "\n")
+                        i = 1
+                else:
+                    with open("./ラクマディレクトリ.txt", 'a', encoding="utf-8") as f:
+                        f.write(d + "\n")
+        except:
+            print("ラクマディレクトリerr")
+            self.logger.debug('ラクマディレクトリerr')
+
+    def ラクマ出品一覧(self):
+        try:
+            with open(self.file_path, 'r', encoding="utf-8") as f:
+                self.seed1 = f.read()
+
+            data = []
+            soup = BeautifulSoup(self.seed1, 'html.parser')
+            data_soup = soup.find_all("div", attrs={"class": "media-body"})
+            for i in data_soup:
+                data.append(i.find("h4", attrs={"class": "media-heading"}))
+
+            #data = data.find("h4", attrs={"class": "media-heading"})
+            
+            # <div class="media-body">
+            # <h4 class="media-heading">新品送料込み　男女兼用　白足袋24.0ｃｍ★成人式、結婚式、卒業式 ATU005</h4>
+            
+            i = 0
+            for d in data:
+                # print(d.text)
+                if i == 0:
+                    # print(str(self.file_out.parent))
+                    with open(str(self.file_out.parent) + "\\ラクマ出品一覧.txt", 'w', encoding="utf-8") as f:
+                        f.write(d.text + "\n")
+                        i = 1
+                else:
+                    with open(str(self.file_out.parent) + "\\ラクマ出品一覧.txt", 'a', encoding="utf-8") as f:
+                        f.write(d.text + "\n")
+        except:
+            print("ラクマ出品一覧err")
+            self.logger.debug('ラクマ出品一覧err')
+
+    def ラクマ登録２重チェック(self):
+        try:
+            with open(str(self.file_out.parent) + "/ラクマ出品一覧.txt", 'r', encoding="utf-8") as f:
+                self.seed1 = f.read().split("\n")
+
+            with open(str(self.file_out.parent) + "/ラクマ出品一覧Ruby.txt", 'r', encoding="utf-8") as f:
+                self.seed2 = f.read().split("\n")
+
+            # listを削除する
+            for i in self.seed2:
+                self.seed1.remove(i)
+
+            i = 0
+            for d in self.seed1:
+                print("====２重チェック====")
+                print(d)
+                if i == 0:
+                    with open(str(self.file_out.parent) + "\\ラクマ出品一覧２重登録.txt", 'w', encoding="utf-8") as f:
+                        f.write(d + "\n")
+                        i = 1
+                else:
+                    with open(str(self.file_out.parent) + "\\ラクマ出品一覧２重登録.txt", 'a', encoding="utf-8") as f:
+                        f.write(d + "\n")
+        except:
+            print("ラクマ登録２重チェックerr")
+            self.logger.debug('ラクマ登録２重チェックerr')
+
+    def 登録出来ていない商品チェック(self):
+        try:
+            with open("./ラクマディレクトリ.txt", 'r', encoding="utf-8") as f:
+                self.seed1 = f.read().split("\n")
+
+            with open(str(self.file_out.parent) + "/ラクマ出品一覧Ruby.txt", 'r', encoding="utf-8") as f:
+                self.seed2 = f.read().split("\n")
+
+            with open(str(self.file_out.parent) + "/ラクマ出品一覧在庫0.txt", 'r', encoding="utf-8") as f:
+                self.seed3 = f.read().split("\n")
+
+            print(self.seed1[0])
+            print(self.seed2[0])
+            print(self.seed3[0])
+            # try:
+            for i in self.seed2:
+                self.seed1.remove(i)
+
+            for i in self.seed3:
+                self.seed1.remove(i)
+            # except:
+                # print("対象がないです")
+
+            i = 0
+            for d in self.seed1:
+                # print("====登録出来ていない商品かも====")
+                # print(d)
+                d_path = pathlib.Path(d)
+                d = d_path.name
+                d = d.replace(".txt", "")
+                if i == 0:
+                    with open(str(self.file_out.parent) + "\\登録出来ていない商品かも.txt", 'w', encoding="utf-8") as f:
+                        f.write(d + "\n")
+                        i = 1
+                else:
+                    with open(str(self.file_out.parent) + "\\登録出来ていない商品かも.txt", 'a', encoding="utf-8") as f:
+                        f.write(d + "\n")
+        except:
+            print("登録出来ていない商品チェックerr")
+            self.logger.debug('登録出来ていない商品チェックerr')
+
+    def 在庫1商品リスト(self):
+        try:
+            with open("./ラクマディレクトリ.txt", 'r', encoding="utf-8") as f:
+                self.seed1 = f.read().split("\n")
+
+            i = 0
+            for t in self.seed1:
+                if "在庫1" in t:
+                    # print("====在庫1====")
+                    # print(t)
+                    d_path = pathlib.Path(t)
+                    d = d_path.name
+                    d = d.replace(".txt", "")
+                    if i == 0:
+                        with open(str(self.file_out.parent) + "\\ラクマ出品一覧在庫1.txt", 'w', encoding="utf-8") as f:
+                            f.write(d + "\n")
+                            i = 1
+                    else:
+                        with open(str(self.file_out.parent) + "\\ラクマ出品一覧在庫1.txt", 'a', encoding="utf-8") as f:
+                            f.write(d + "\n")
+        except:
+            print("在庫1商品リストerr")
+            self.logger.debug('在庫1商品リストerr')
+
+    def 在庫0商品リスト(self):
+        try:
+            with open("./ラクマディレクトリ.txt", 'r', encoding="utf-8") as f:
+                self.seed1 = f.read().split("\n")
+
+            i = 0
+            for t in self.seed1:
+                if "在庫0" in t:
+                    #print("====在庫0====")
+                    #print(t)
+                    d_path = pathlib.Path(t)
+                    d = d_path.name
+                    d = d.replace(".txt", "")
+                    if i == 0:
+                        with open(str(self.file_out.parent) + "\\ラクマ出品一覧在庫0.txt", 'w', encoding="utf-8") as f:
+                            f.write(d + "\n")
+                            i = 1
+                    else:
+                        with open(str(self.file_out.parent) + "\\ラクマ出品一覧在庫0.txt", 'a', encoding="utf-8") as f:
+                            f.write(d + "\n")
+        except:
+            print("在庫0商品リストerr")
+            self.logger.debug('在庫0商品リストerr')
+
+    def load(self, dir):
+        with open(dir, 'r', encoding="utf-8") as f:
+            self.seed1 = f.read()
+        self.seed1 = self.seed1.replace("\n", "")
+        self.seed1 = self.seed1.replace("\r", "")
+        return self.seed1
+
+    def 出品されているのに在庫0のリスト(self):
+        try:
+            with open(str(self.file_out.parent) + "/ラクマ出品一覧Ruby.txt", 'r', encoding="utf-8") as f:
+                self.seed1 = f.read().split("\n")
+
+            with open(str(self.file_out.parent) + "/ラクマ出品一覧在庫0.txt", 'r', encoding="utf-8") as f:
+                self.seed2 = f.read().split("\n")
+
+            data = []
+            for i in self.seed1:
+                for s in self.seed2:
+                    if i == s:
+                        data.append(i)
+
+            i = 0
+            for d in data:
+
+                # print("====出品されているのに在庫0のリスト====")
+                # print(d)
+
+                if i == 0:
+                    with open(str(self.file_out.parent) + "\\出品されているのに在庫0のリスト.txt", 'w', encoding="utf-8") as f:
+                        f.write(d + "\n")
+                        i = 1
+                else:
+                    with open(str(self.file_out.parent) + "\\出品されているのに在庫0のリスト.txt", 'a', encoding="utf-8") as f:
+                        f.write(d + "\n")
+        except:
+            print("出品されているのに在庫0のリストerr")
+            self.logger.debug('出品されているのに在庫0のリストerr')
+
+    def 出品されているB品のリスト(self):
+        print("")
+    
+    def 出品されている在庫1のリスト(self):
+            print("")
+
+    def 商品コードが認識されなかったリスト(self):
+            print("")
+
+    def 出品の比較する(self):
+        try:
+            # 比較_元
+            with open(str(self.file_out.parent) + "/ラクマ出品一覧.txt", 'r', encoding="utf-8") as f:
+                self.seed1 = f.read().split("\n")
+
+            # 比較先
+            with open(str(self.file_out2.parent) + "/ラクマ出品一覧.txt", 'r', encoding="utf-8") as f:
+                self.seed2 = f.read().split("\n")
+
+            data = []
+            for i in self.seed1:
+                for s in self.seed2:
+                    if i == s:
+                        data.append(i)
+
+            i = 0
+            for d in data:
+
+                # print("====出品の比較する====")
+                # print(d)
+
+                if i == 0:
+                    with open(str(self.file_out2.parent) + "\\出品の比較する.txt", 'w', encoding="utf-8") as f:
+                        f.write(d + "\n")
+                        i = 1
+                else:
+                    with open(str(self.file_out2.parent) + "\\出品の比較する.txt", 'a', encoding="utf-8") as f:
+                        f.write(d + "\n")
+        except:
+            print("出品の比較するerr")
+            self.logger.debug('出品の比較するerr')
+
+    def get_logger(logger_name, log_file, f_fmt='%(message)s'):
+        """ロガーを取得"""
+        # ロガー作成
+        logger = logging.getLogger(logger_name)
+        logger.setLevel(logging.DEBUG)
+
+        # ファイルハンドラ作成
+        file_handler = logging.FileHandler(
+            log_file, mode='a', encoding='utf-8')
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(logging.Formatter(f_fmt))
+
+        # ロガーに追加
+        logger.addHandler(file_handler)
+
+        return logger
+
+
+if __name__ == '__main__':
+    rakuma_test = Rakuma_Test()
+
+    # while True:
+    #    rakuma_test.main()
+    #    time.sleep(10)
---
+++ GUI/在庫管理/zaラクマ_入力テスト.rb
@@ -0,0 +1,258 @@
+# -*- coding:utf-8 -*-
+
+# 2021/08/08
+
+ROOT_PATH = 'C:/Users/user/Downloads/バックアップ/プログラム/バックアップ/保存/メルカリ'
+
+
+class Rakuma_Test
+    def initialize()
+
+        @path = ARGV[0]
+        @data = []
+        File.open(@path + "/ラクマ出品一覧.txt", "r:utf-8") do |f|
+            @data = f.read().split("\n")
+        end
+
+        self.二重登録チェック()
+		self.二重登録チェック2()
+        #ソートする()
+    end
+
+    def ソートする()
+        begin
+            File.open(@path + "/ラクマ出品一覧.txt", "r:utf-8") do |f|
+                @data = f.read().split("\n")
+            end
+            @data.sort!
+            i = 0
+            @data.each{|data|
+                if i == 0 then
+                    File.open(@path + "/ラクマ出品一覧.txt", "w:utf-8") do |f|
+                        f.write(data + "\n")
+                        i = 1
+                    end
+                else
+                    File.open(@path + "/ラクマ出品一覧.txt", "a:utf-8") do |f|
+                        f.write(data + "\n")
+                    end
+                end
+            }
+        end
+
+        begin
+            File.open(@path + "/ラクマ出品一覧Ruby.txt", "r:utf-8") do |f|
+                @data = f.read().split("\n")
+            end
+            @data.sort!
+            i = 0
+            @data.each{|data|
+                if i == 0 then
+                    File.open(@path + "/ラクマ出品一覧Ruby.txt", "w:utf-8") do |f|
+                        f.write(data + "\n")
+                        i = 1
+                    end
+                else
+                    File.open(@path + "/ラクマ出品一覧Ruby.txt", "a:utf-8") do |f|
+                        f.write(data + "\n")
+                    end
+                end
+            }
+        end
+
+        begin
+=begin
+            File.open("./ラクマディレクトリ.txt", "r:utf-8") do |f|
+                @data = f.read().split("\n")
+            end
+            @data.sort!
+            i = 0
+            @data.each{|data|
+                if i == 0 then
+                    File.open("./ラクマディレクトリ.txt", "w:utf-8") do |f|
+                        f.write(data + "\n")
+                        i = 1
+                    end
+                else
+                    File.open("./ラクマディレクトリ.txt", "a:utf-8") do |f|
+                        f.write(data + "\n")
+                    end
+                end
+            }
+=end
+            t = 0
+            Dir.glob(ROOT_PATH + "/**/*.txt").each{|i|
+                if t == 0 then
+                    File.open("./ラクマディレクトリ.txt", "w:utf-8") do |f|
+                        f.write(i + "\n")
+                    end
+                    t = 1
+                else
+                    File.open("./ラクマディレクトリ.txt", "a:utf-8") do |f|
+                        f.write(i + "\n")
+                    end
+                end
+            }
+            File.open("./ラクマディレクトリ.txt", "r:utf-8") do |f|
+                @data = f.read().split("\n")
+            end
+            @data.sort!
+            File.open("./ラクマディレクトリ.txt", "w:utf-8") do |f|
+                f.write(@data)
+            end
+		rescue
+			
+        end
+
+        begin
+            File.open(@path + "/ラクマ出品一覧在庫1.txt", "r:utf-8") do |f|
+                @data = f.read().split("\n")
+            end
+            @data.sort!
+            i = 0
+            @data.each{|data|
+                if i == 0 then
+                    File.open(@path + "/ラクマ出品一覧在庫1.txt", "w:utf-8") do |f|
+                        f.write(data + "\n")
+                        i = 1
+                    end
+                else
+                    File.open(@path + "/ラクマ出品一覧在庫1.txt", "a:utf-8") do |f|
+                        f.write(data + "\n")
+                    end
+                end
+            }
+        end
+
+        begin
+            File.open(@path + "/ラクマ出品一覧在庫0.txt", "r:utf-8") do |f|
+                @data = f.read().split("\n")
+            end
+            @data.sort!
+            i = 0
+            @data.each{|data|
+                if i == 0 then
+                    File.open(@path + "/ラクマ出品一覧在庫0.txt", "w:utf-8") do |f|
+                        f.write(data + "\n")
+                        i = 1
+                    end
+                else
+                    File.open(@path + "/ラクマ出品一覧在庫0.txt", "a:utf-8") do |f|
+                        f.write(data + "\n")
+                    end
+                end
+            }
+        end
+
+        begin
+            File.open(@path + "/ラクマ出品一覧２重登録.txt", "r:utf-8") do |f|
+                @data = f.read().split("\n")
+            end
+            @data.sort!
+            i = 0
+            @data.each{|data|
+                if i == 0 then
+                    File.open(@path + "/ラクマ出品一覧２重登録.txt", "w:utf-8") do |f|
+                        f.write(data + "\n")
+                        i = 1
+                    end
+                else
+                    File.open(@path + "/ラクマ出品一覧２重登録.txt", "a:utf-8") do |f|
+                        f.write(data + "\n")
+                    end
+                end
+            }
+        end
+
+        begin
+            File.open(@path + "/登録出来ていない商品かも.txt", "r:utf-8") do |f|
+                @data = f.read().split("\n")
+            end
+            @data.sort!
+            i = 0
+            @data.each{|data|
+                if i == 0 then
+                    File.open(@path + "/登録出来ていない商品かも.txt", "w:utf-8") do |f|
+                        f.write(data + "\n")
+                        i = 1
+                    end
+                else
+                    File.open(@path + "/登録出来ていない商品かも.txt", "a:utf-8") do |f|
+                        f.write(data + "\n")
+                    end
+                end
+            }
+        end
+
+        begin
+            File.open(@path + "/出品されているのに在庫0のリスト.txt", "r:utf-8") do |f|
+                @data = f.read().split("\n")
+            end
+            @data.sort!
+            i = 0
+            @data.each{|data|
+                if i == 0 then
+                    File.open(@path + "/出品されているのに在庫0のリスト.txt", "w:utf-8") do |f|
+                        f.write(data + "\n")
+                        i = 1
+                    end
+                else
+                    File.open(@path + "/出品されているのに在庫0のリスト.txt", "a:utf-8") do |f|
+                        f.write(data + "\n")
+                    end
+                end
+            }
+        end
+    end
+
+    def 二重登録チェック()
+        data = @data
+        dammy = @data
+        dammy.uniq!
+
+        s = 0
+        #dsize = data.size()
+        dammy.each{|i|
+            if s == 0 then
+                File.open(@path + "/ラクマ出品一覧Ruby.txt", "w:utf-8") do |f|
+                    f.write(i + "\n")
+                end
+                s = 1
+            else
+                File.open(@path + "/ラクマ出品一覧Ruby.txt", "a:utf-8") do |f|
+                    f.write(i + "\n")
+                end
+            end
+        }
+    end
+	
+	    def 二重登録チェック2()
+        data = @data
+        dammy = @data
+        dammy.uniq!
+
+		dammy = data - dammy
+        s = 0
+        #dsize = data.size()
+        dammy.each{|i|
+            if s == 0 then
+                File.open(@path + "/ラクマ出品一覧２重登録2.txt", "w:utf-8") do |f|
+                    f.write(i + "\n")
+                end
+                s = 1
+            else
+                File.open(@path + "/ラクマ出品一覧２重登録2.txt", "a:utf-8") do |f|
+                    f.write(i + "\n")
+                end
+            end
+        }
+		if dammy == data then
+			    File.open(@path + "/ラクマ出品一覧２重登録2.txt", "w:utf-8") do |f|
+                    f.write("ありませんでした。" + "\n")
+                end
+		end
+    end
+end
+
+rakuma_test = Rakuma_Test.new()
+ 
