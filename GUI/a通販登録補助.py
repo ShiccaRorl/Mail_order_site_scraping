28a98d0adefa0e3f255795a1d123cba62d17fe63
ShiccaRorl <shicca.rorl@gmail.com>
2021-11-20 22:44:33 +0900

    補助登録追加

--- GUI/a通販登録補助.py
+++ GUI/a通販登録補助.py
@@ -29,45 +29,55 @@ id = 1
 seeds = config.Base.classes.seeds_seeds
 
 
-上= [
-  [sg.Text('通販登録補助管理', size=(13,1))],
-  [sg.Text('ソース', size=(10,1)), sg.Multiline(default_text="", key="-ソース-", size=(70, 7))],
-  [sg.Text('柄あり', size=(10,1)), sg.Checkbox("固定", key="-固定-")],
-  [sg.Text('バリエーションあり', size=(10,1)),sg.Checkbox("固定", key="-固定-")],
-]
+上 = sg.Column([
+    [sg.Text('通販登録補助管理', size=(13,1))],
+    [sg.Text('ソース', size=(10,1)), sg.Multiline(default_text="", key="-ソース-", size=(70, 7))],
+    [sg.Text('柄あり', size=(10,1)), sg.Checkbox("固定", key="-固定-")],
+    [sg.Text('バリエーションあり', size=(10,1)),sg.Checkbox("固定", key="-固定-")],
+    ])
 
-左 = [
-  [sg.Text('商品', size=(13,1))],
-  [sg.Text('商品コード', size=(10,1)),sg.Checkbox("固定", key="-固定-"), sg.InputText('', key="-商品コード-", size=(40,1)), sg.Button(button_text='コピー', key="-コピー-")],
-  [sg.Text('商品名', size=(10,1)),sg.Checkbox("固定", key="-固定-"), sg.InputText('', key="-商品名-", size=(40,1)),sg.Button(button_text='コピー', key="-コピー-") ],
-  [sg.Text('税抜き価格', size=(10,1)),sg.Checkbox("固定", key="-固定-"), sg.InputText('', key="-商品名-", size=(40,1)),sg.Button(button_text='コピー', key="-コピー-")],
-  [sg.Text('税送料込み', size=(10,1)),sg.Checkbox("固定", key="-固定-"), sg.InputText('', key="-商品名-", size=(40,1)),sg.Button(button_text='コピー', key="-コピー-")],
+左 = sg.Column([
+    [sg.Text('商品', size=(13, 1))],
+    [sg.Text('商品コード', size=(10, 1)), sg.Checkbox("固定", key="-固定-"), sg.InputText('', key="-商品コード-", size=(40, 1)),],
+    [sg.Text('商品名', size=(10, 1)), sg.Checkbox("固定", key="-固定-"), sg.InputText('', key="-商品名-", size=(40, 1)),],
+    [sg.Text('税抜き価格', size=(10, 1)), sg.Checkbox("固定", key="-固定-"), sg.InputText('', key="-税抜き価格-", size=(40, 1)),],
+    [sg.Text('税送料込み', size=(10, 1)), sg.Checkbox("固定", key="-固定-"), sg.InputText('', key="-税送料込み-", size=(40, 1)),],
 
-  [sg.Text('発送方法', size=(13,1))],
-  [sg.Text('ヤフオク', size=(10,1)),sg.Checkbox("固定", key="-固定-"), sg.InputText('', key="-商品名-", size=(40,1)), sg.Button(button_text='コピー', key="-コピー-")],
-  [sg.Text('アマゾン', size=(10,1)),sg.Checkbox("固定", key="-固定-"), sg.InputText('', key="-商品名-", size=(40,1)), sg.Button(button_text='コピー', key="-コピー-")],
-  [sg.Text('楽天', size=(10,1)),sg.Checkbox("固定", key="-固定-"), sg.InputText('', key="-商品名-", size=(40,1)), sg.Button(button_text='コピー', key="-コピー-")],
-  [sg.Text('メルカリ', size=(10,1)),sg.Checkbox("固定", key="-固定-"), sg.InputText('', key="-商品名-", size=(40,1)), sg.Button(button_text='コピー', key="-コピー-")],
-  [sg.Text('ラクマ', size=(10,1)),sg.Checkbox("固定", key="-固定-"), sg.InputText('', key="-商品名-", size=(40,1)), sg.Button(button_text='コピー', key="-コピー-")],
+    [sg.Text('発送方法', size=(13, 1))],
+    [sg.Text('ストアクリエイター', size=(10, 1)), sg.Checkbox("固定", key="-固定-"), sg.InputText('', key="-商品名-", size=(40, 1)),],
+    [sg.Text('アマゾン', size=(10, 1)), sg.Checkbox("固定", key="-固定-"), sg.InputText('', key="-商品名-", size=(40, 1)),],
+    [sg.Text('楽天', size=(10, 1)), sg.Checkbox("固定", key="-固定-"), sg.InputText('', key="-商品名-", size=(40, 1)),],
+    [sg.Text('メルカリ', size=(10, 1)), sg.Checkbox("固定", key="-固定-"), sg.InputText('', key="-商品名-", size=(40, 1)),],
+    [sg.Text('ラクマ', size=(10, 1)), sg.Checkbox("固定", key="-固定-"), sg.InputText('', key="-商品名-", size=(40, 1)),],
 
-  [sg.Text('商品説明', size=(10,1))],
-  [sg.Text('サイズ', size=(10,1)),sg.Checkbox("固定", key="-固定-"), sg.InputText('', key="-サイズ-", size=(40,1)), sg.Button(button_text='コピー', key="-コピー-")],
-  [sg.Text('品質', size=(10,1)),sg.Checkbox("固定", key="-固定-"), sg.InputText('', key="-品質-", size=(40,1)), sg.Button(button_text='コピー', key="-コピー-")],
-  [sg.Text('生産国', size=(10,1)),sg.Checkbox("固定", key="-固定-"), sg.InputText('', key="-生産国-", size=(40,1)), sg.Button(button_text='コピー', key="-コピー-")],
- 
-  ]
+    [sg.Text('商品説明', size=(10, 1))],
+    [sg.Text('サイズ', size=(10, 1)), sg.Checkbox("固定", key="-固定-"), sg.InputText('', key="-サイズ-", size=(40, 1)),],
+    [sg.Text('品質', size=(10, 1)), sg.Checkbox("固定", key="-固定-"), sg.InputText('', key="-品質-", size=(40, 1)),],
+    [sg.Text('生産国', size=(10, 1)), sg.Checkbox("固定", key="-固定-"), sg.InputText('', key="-生産国-", size=(40, 1)),],
+    [sg.Text('', size=(10,1)),sg.Button(button_text='保存', key="-保存-")],
+])
 
-下 = [
-[sg.Text('過去ログ', size=(10,1)), sg.Listbox([0,0], enable_events=True, size=(70, 7), key='-過去ログ-')],
-]
+下 = sg.Column([
+    [sg.Text('過去ログ', size=(10,1)), sg.Listbox([0,0], enable_events=True, size=(70, 7), key='-過去ログ-')],
+    ])
 
   
-右 = [
+右 = sg.Column([
     [sg.Text('操作', size=(10,1)),sg.Button(button_text='上にずらす', key="-上にずらす-")],
     [sg.Text('', size=(10,1)),sg.Button(button_text='下にずらす', key="-下にずらす-")],
-]
+    [sg.Text('操作', size=(10,1))],
+    [sg.Text('', size=(10,1)),sg.Button(button_text='Amazon', key="-Amazon-")],
+    [sg.Text('', size=(10, 1)), sg.Button(button_text='ストアクリエイター', key="-ストアクリエイター-")],
+    [sg.Text('', size=(10, 1)), sg.Button(button_text='楽天', key="-楽天-")],
+    [sg.Text('', size=(10, 1)), sg.Button(button_text='ラクマ', key="-ラクマ-")],
+    [sg.Text('', size=(10, 1)), sg.Button(button_text='メルカリ', key="-メルカリ-")],
+    ])
 
-main = [[]]
+main = [
+    [sg.Pane([上], orientation='h')],
+    [sg.Pane([左, 右], orientation='h')],
+    [sg.Pane([下], orientation='h')],
+    ]
 
 # ウィンドウを作成する
 window = sg.Window('通販登録補助管理', main, resizable=True)
@@ -79,7 +89,7 @@ timebox = Timebox()
 while True:
     event, values = window.read()
 # ユーザーが終了したいのか、ウィンドウが閉じられたかどうかを確認してください
-    if event == sg.WINDOW_CLOSED or event == '終了' or event == "閉じるb":
+    if event == sg.WINDOW_CLOSED or event == '終了' or event == "閉じる":
         break
     elif event == "日付":
         window["-日付-"].update(timebox.in_to_text_day(datetime.date.today()))
