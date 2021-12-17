418c78744cafae67bdf874e8f72606742e952ba3
ShiccaRorl <shicca.rorl@gmail.com>
2021-12-17 12:36:32 +0900

    uu

--- GUI/在庫管理/aラクマ出品_GUI.py
+++ GUI/在庫管理/aラクマ出品_GUI.py
@@ -21,11 +21,11 @@ from sqlalchemy.orm import sessionmaker
 from config import Config
 #help(sg.Table)
 
-config = Config()
+#config = Config()
 
 
-sale_sele = config.Base.classes.sale_sele
-
+#sale_sele = config.Base.classes.sale_sele
+"""
 def max_id():
     # 過去ログテーブル　の計算
     session = Session(config.engine)
@@ -42,7 +42,7 @@ def max_id():
 
 
 id = max_id()
-
+"""
 
 def 取引リスト():
     return ['test', 'test']
@@ -51,32 +51,34 @@ def 取引リスト():
 
 ヘッダー = sg.Column([
     [sg.Text('ラクマEditer')],
+    [sg.InputText('', key="-商品名-", size=(20, 1)), sg.Button(button_text='データ取り込み',key="-データ取り込み-")],
     [sg.Text('並び替え')],
     [sg.Button(button_text='そのまま',key="-そのまま-"), sg.Button(button_text='B品順',key="-B品順-"),
-     sg.Button(button_text='在庫1',key="-在庫1-"), sg.Button(button_text='売れ筋??',key="-売れ筋-"),],
+     sg.Button(button_text='在庫1',key="-在庫1-"), sg.Button(button_text='売れ筋??',key="-売れ筋-"), sg.Button(button_text='ソート',key="-ソート-"),],
     [sg.Text('商品リスト')],
-    [sg.Listbox(商品リスト(), enable_events=True, size=(50, 200), key='-商品リスト-')],
-    [sg.InputText('', key="-id-", size=(3,1)), sg.InputText('', key="-商品名-", size=(5, 1))],
+    #[sg.Listbox(商品リスト(), enable_events=True, size=(50, 200), key='-商品リスト-')],
+    [sg.InputText('', key="-商品名-", size=(10, 1))],
 ])
 
 中央 = sg.Column([
-  [sg.Text('ラクマEditer')],
-  [sg.Text('ID', size=(10,1)), sg.InputText(id, key="-ID-", size=(10,1))],
-  [sg.Text('日付', size=(10,1)), sg.InputText('', key="-日付-", size=(20,1)), sg.Button(button_text='日付',key="-日付-")],
-  [sg.Text('ファイルデータベース取り込み', size=(10,1)), sg.Button(button_text='更新',key="-更新-")],
+  #[sg.Text('ラクマEditer')],
+  #[sg.Text('ID', size=(10,1)), sg.InputText(id, key="-ID-", size=(10,1))],
+  [sg.Text('日付', size=(10,1)), sg.InputText(datetime.date.today(), key="-日付-", size=(20,1)), sg.Button(button_text='日付',key="-日付-")],
+  #[sg.Text('ファイルデータベース取り込み', size=(10,1)), sg.Button(button_text='更新',key="-更新-")],
+  [sg.Button(button_text='<=====',key="<====="), sg.Button(button_text='フォルダ開く',key="フォルダ開く"), sg.Button(button_text='=====>',key="=====>"),],
 ])
 
 
 
 
 フッター = sg.Column([
-  [sg.Text('コメント', size=(10,1)), sg.Multiline(default_text="", size=(50, 5), key="-コメント-"), sg.Button(button_text='新規保存', key="-新規保存-"), sg.Button(button_text='保存', key="-保存-"), sg.Button(button_text='読了', key="-読了-"), sg.Button(button_text='閉じる', key="-閉じる-")],
-  [sg.Text('過去ログ', size=(10,1)), sg.Listbox(過去ログ(), enable_events=True, size=(200, 10), key='-過去ログ-'), sg.Button(button_text='読み込み', key="-読み込み-")],
+  #[sg.Text('コメント', size=(10,1)), sg.Multiline(default_text="", size=(50, 5), key="-コメント-"), sg.Button(button_text='新規保存', key="-新規保存-"), sg.Button(button_text='保存', key="-保存-"), sg.Button(button_text='読了', key="-読了-"), sg.Button(button_text='閉じる', key="-閉じる-")],
+  #[sg.Text('過去ログ', size=(10,1)), sg.Listbox(過去ログ(), enable_events=True, size=(200, 10), key='-過去ログ-'), sg.Button(button_text='読み込み', key="-読み込み-")],
   ])
 
 L = [
-    #[sg.Pane([ヘッダー])],
-    [sg.Pane([メイン])],
+    [sg.Pane([ヘッダー])],
+    [sg.Pane([中央])],
     [sg.Pane([フッター])],
     ]
 
@@ -84,7 +86,7 @@ L = [
 window = sg.Window('ラクマEditor', L, resizable=True)
 
 # == 時間↓ ==
-timebox = Timebox()
+#timebox = Timebox()
 
 # イベントループを使用してウィンドウを表示し、対話する
 while True:
@@ -92,6 +94,8 @@ while True:
     # ユーザーが終了したいのか、ウィンドウが閉じられたかどうかを確認してください
     if event == sg.WINDOW_CLOSED or event == '終了' or event == "閉じるb":
         break
+        
+    """
     elif event == "日付":
         window["-日付-"].update(timebox.in_to_text_day(datetime.date.today()))
         日付 = datetime.date.today()
@@ -127,6 +131,7 @@ while True:
         window["-読了-"].update("読了")
     elif event == "-閉じる-":
         break
+    """
 
     print(event, values)
     # Output a message to the window
